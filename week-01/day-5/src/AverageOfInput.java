4abc02c0bc2c8474d07137d176edb4da9cc51970
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-11 10:57:39 +0200

    Average of Input task

---
+++ week-01/day-5/src/AverageOfInput.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class AverageOfInput {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.print("Please enter the first number: ");
+        int userInput1 = scanner.nextInt();
+        System.out.print("Please enter the first number: ");
+        int userInput2 = scanner.nextInt();
+        System.out.print("Please enter the first number: ");
+        int userInput3 = scanner.nextInt();
+        System.out.print("Please enter the first number: ");
+        int userInput4 = scanner.nextInt();
+        System.out.print("Please enter the first number: ");
+        int userInput5 = scanner.nextInt();
+
+        int sum = userInput1 + userInput2 + userInput3 + userInput4 + userInput5;
+        double average = (userInput1 + userInput2 + userInput3 + userInput4 + userInput5) / 5;
+        System.out.println("The sum of these numbers is: " + sum);
+        System.out.println("The average of these numbers is: " + average);
+
+    }
+}
