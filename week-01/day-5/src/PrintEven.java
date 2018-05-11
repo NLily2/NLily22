3831ea53acddf2a1bc8c6958e4b0e700b3cb2743
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-11 14:59:04 +0200

    PrintEvent task

---
+++ week-01/day-5/src/PrintEven.java
@@ -0,0 +1,12 @@
+public class PrintEven {
+    public static void main(String[] args) {
+
+        int num2 = 500;
+
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
