a134bc9778b6e11ec19eb84a99dafce0da9990cf
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-11 11:54:48 +0200

    IwontCheatOnTheExam Task

---
+++ week-01/day-5/src/IWontCheatOnTheExams.java
@@ -0,0 +1,14 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+
+        int num = 100;
+        int n = 1;
+
+    while (n <= num) {
+        System.out.println("I won't cheat on the exam!");
+        n++;
+    }
+
+
+    }
+}
