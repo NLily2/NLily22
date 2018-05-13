0c29c3233b7bf3ad29e20a141b441a81217e8178
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-13 15:39:58 +0200

    PrintEven modified

--- week-01/day-5/src/PrintEven.java
+++ week-01/day-5/src/PrintEven.java
@@ -3,7 +3,7 @@ public class PrintEven {
 
         int num2 = 500;
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 500; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
