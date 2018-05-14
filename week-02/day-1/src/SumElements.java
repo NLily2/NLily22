3642e43387e3f38f82b7f941eaca1bc5700773d0
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-14 15:27:34 +0200

    SumElements Task

--- week-02/day-1/src/SumElements.java
+++ week-02/day-1/src/SumElements.java
@@ -1,7 +1,7 @@
 public class SumElements {
     public static void main(String[] args) {
      int r[] = {54, 23, 66, 12};
-     int sum = r[2] + r[3];
+     int sum = r[1] + r[2];
         System.out.println(sum);
 
     }
