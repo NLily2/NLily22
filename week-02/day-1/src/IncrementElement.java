40a9027a7cca29994da21b01f25b11009fd40225
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-14 15:26:11 +0200

    IncrementElement Task

--- week-02/day-1/src/IncrementElement.java
+++ week-02/day-1/src/IncrementElement.java
@@ -6,10 +6,3 @@ public class IncrementElement {
         System.out.println(t[2]);
     }
 }
-
-
-
-/*Create an array variable named `t`
-        with the following content: `[1, 2, 3, 4, 5]`
-        - Increment the third element
-        - Print the third element   */
