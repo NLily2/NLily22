d0fe41d7899e87c96014fd2c1aff8a5d72614e9f
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-15 21:23:56 +0200

    SimpleReplace Task

---
+++ week-02/day-2/src/SimpleReplace.java
@@ -0,0 +1,8 @@
+public class SimpleReplace {
+  public static void main (String[] args) {
+
+    String example = "In a dishwasher far far away";
+    String exampleone = example.replace("dishwasher", "galaxy");
+    System.out.println(exampleone);
+  }
+}
