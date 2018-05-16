eb45d75b8306eaff7c6d64ab768089bc4f8f030d
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-16 12:52:32 +0200

    PrintEachLine Task

---
+++ week-02/day-3/workshop/src/PrintEachLine.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
+public class PrintEachLine {
+  public static void main(String[] args) {
+
+    String fileName = "my-file.txt";
+
+    try {
+    Path filePath = Paths.get("my-file.txt");
+      List<String>lines = Files.readAllLines(filePath);
+      for (int i =1; i < lines.size(); i++) {
+        System.out.println(lines.get(i));
+      }
+    } catch (IOException e){
+      System.out.println("Unable to read file: my-file.txt");
+    }
+  }
+}
