8e0d4beb076b436ec260bd5c346b3012621c604f
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-20 23:18:14 +0200

    Cuboid Task

---
+++ week-01/practiceRemainedExercises/Expressions_and_Control_Flow/Cuboid.java
@@ -0,0 +1,14 @@
+public class Cuboid {
+  public static void main(String[] args) {
+
+    double side1 = 5;
+    double side2 = 10;
+    double side3 = 15;
+    double surface = 0;
+    double volume = 0;
+
+    System.out.println(surface = ((side1 * side2) * 2) + ((side1 * side3) * 2) + ((side2 * side3) * 2));
+    System.out.println(volume = side1 * side2 * side3);
+
+  }
+}
