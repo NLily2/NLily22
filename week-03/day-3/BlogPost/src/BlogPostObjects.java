78ed67c52c6959c383b2cb1e11757e0a7207868a
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-27 11:23:14 +0200

    BlogPost task

--- week-03/day-3/BlogPost/src/BlogPostObjects.java
+++ week-03/day-3/BlogPost/src/BlogPostObjects.java
@@ -18,6 +18,5 @@ public class BlogPostObjects {
     post3.publicationDate = "2017.03.28.";
     post3.title = "\"One Engineer Is Trying to Get IBM to Reckon With Trump\"";
     post3.text = "Daniel Hanley, a cybersecurity engineer at IBM, doesn’t want to be the center of attention. When I asked to take his picture outside one of IBM’s New York City offices, he told me that he wasn’t really into the whole organizer profile thing.";
-
   }
 }
