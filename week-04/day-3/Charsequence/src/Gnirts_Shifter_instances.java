07356d9f688032165b30bc10621eee77477f175a
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-30 14:44:41 +0200

    creating Gnirts_Shifter_instances class and trying out new charAt function

---
+++ week-04/day-3/Charsequence/src/Gnirts_Shifter_instances.java
@@ -0,0 +1,7 @@
+public class Gnirts_Shifter_instances {
+  public static void main(String[] args) {
+    Gnirts example = new Gnirts("example");
+
+    System.out.println(example.charAt(2));
+  }
+}
