f241d0daba525767ff71ae96b1e41066d4886ea1
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-30 11:57:09 +0200

    trying out the clone function

--- week-04/day-3/clonable/src/Student_instances.java
+++ week-04/day-3/clonable/src/Student_instances.java
@@ -2,6 +2,7 @@ public class Student_instances {
   public static void main(String[] args) {
 
     Student John = new Student("John", 20, "male", "BME");
-    
+    John.clone().introduce();
+    John.introduce();
   }
 }
