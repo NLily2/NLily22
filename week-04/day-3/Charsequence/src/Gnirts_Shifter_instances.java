dee6ad326e22681d709c98c84401763ad1dbd8af
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-05-30 14:50:53 +0200

    trying out new charAt function of Shifter class

--- week-04/day-3/Charsequence/src/Gnirts_Shifter_instances.java
+++ week-04/day-3/Charsequence/src/Gnirts_Shifter_instances.java
@@ -3,5 +3,8 @@ public class Gnirts_Shifter_instances {
     Gnirts example = new Gnirts("example");
 
     System.out.println(example.charAt(2));
+
+    Shifter newexample = new Shifter("newexample", 2);
+    System.out.println(newexample.charAt(4));
   }
 }
