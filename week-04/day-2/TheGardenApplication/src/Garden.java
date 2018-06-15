12e72494d81e3efb36d49b85d566d3fa8a19e84a
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-06-15 17:49:10 +0200

    create garden object

--- week-04/day-2/TheGardenApplication/src/Garden.java
+++ week-04/day-2/TheGardenApplication/src/Garden.java
@@ -1,40 +1,39 @@
 import java.util.ArrayList;
-
 public class Garden {
-  ArrayList<Plant> plantInGarden;
 
-  Flower flower1 = new Flower("yellow");
-  Flower flower2 = new Flower("blue");
-  Tree tree1 = new Tree("purple");
-  Tree tree2 = new Tree("orange");
+    ArrayList<Plant> plantInGarden = new ArrayList<>();
 
-  public void addPlantsToGarden() {
-    plantInGarden = new ArrayList<>();
-    plantInGarden.add(flower1);
-    plantInGarden.add(flower2);
-    plantInGarden.add(tree1);
-    plantInGarden.add(tree2);
-  }
+    public Garden(){
+    Flower flower1 = new Flower("yellow");
+    Flower flower2 = new Flower("blue");
+    Tree tree1 = new Tree("purple");
+    Tree tree2 = new Tree("orange");
 
-public void waterGarden(double waterUsedForWatering){
-  System.out.println("Watering with " + waterUsedForWatering);
-  int plantNeedWater = 0;
-  Plant temp;
-  for (int i = 0; i < plantInGarden.size(); i++){
-    temp = plantInGarden.get(i);
-    if(temp.waterStatus < temp.waterAmountNeeded){
-      plantNeedWater += 1;
+      plantInGarden.add(flower1);
+      plantInGarden.add(flower2);
+      plantInGarden.add(tree1);
+      plantInGarden.add(tree2);
+    }
+
+    public void waterGarden (double waterUsedForWatering){
+      System.out.println("Watering with " + waterUsedForWatering);
+      int plantNeedWater = 0;
+      Plant temp;
+      for (int i = 0; i < plantInGarden.size(); i++) {
+        temp = plantInGarden.get(i);
+        if (temp.waterStatus < temp.waterAmountNeeded) {
+          plantNeedWater += 1;
+        }
+      }
+      waterUsedForWatering = waterUsedForWatering / plantNeedWater;
+      for (int i = 0; i < plantInGarden.size(); i++) {
+        plantInGarden.get(i).getWatered(waterUsedForWatering);
+      }
+    }
+
+    public void getPlantsStatus (){
+      for (Plant plant : plantInGarden) {
+        plant.getWaterStatus();
+      }
     }
   }
-  waterUsedForWatering = waterUsedForWatering / plantNeedWater;
-  for (int i = 0; i < plantInGarden.size(); i++){
-    plantInGarden.get(i).getWatered(waterUsedForWatering);
-  }
-}
-
-public void getPlantsStatus(){
-    for(Plant plant : plantInGarden){
-      plant.getWaterStatus();
-    }
-  }
-}
--- week-04/day-2/TheGardenApplication/src/GardenApp.java
+++ week-04/day-2/TheGardenApplication/src/GardenApp.java
@@ -1,5 +1,14 @@
 public class GardenApp {
+  public static void main(String[] args) {
 
+    Garden garden = new Garden();
 
-
+    garden.getPlantsStatus();
+    System.out.print("\n");
+    garden.waterGarden(40);
+    garden.getPlantsStatus();
+    System.out.print("\n");
+    garden.waterGarden(70);
+    garden.getPlantsStatus();
+  }
 }
