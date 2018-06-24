e85e5f94b0da6233f9dbe6bf25adec52e8ebe7bc
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-06-24 16:56:45 +0200

    finish exercise5

--- week-06/day-1/Ex5/src/Ex5.java
+++ week-06/day-1/Ex5/src/Ex5.java
@@ -1,23 +1,21 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class Ex5 {
   public static void main(String[] args) {
 
-    String example = "Let's find UpperCase Characters";
-
+    String textWithUppercaseLetters = "Let's find UpperCase Characters";
+    System.out.println(getUppercaseCharacters(textWithUppercaseLetters));
   }
 
   public static String getUppercaseCharacters (String word){
     String[] splittedString = word.split("");
-     Arrays.stream(splittedString)
+     return Arrays.stream(splittedString)
              .map(c -> c.charAt(0))
              .filter(c -> c.isUpperCase(c))
-
-
+             .map(c -> c.toString(c))
+             .reduce((x, y) -> x + y)
+             .get();
   }
-
 }
-
-
-//Write a Stream Expression to find the uppercase characters in a string!
