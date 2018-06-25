1e11cf688339cbb29c937a611d6ebdc94ba8c09b
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-06-25 15:48:33 +0200

    add REST Greet counter app

---
+++ week-07/day1/src/main/java/com/greenfoxacademy/springstart/controllers/AtomicLong.java
@@ -0,0 +1,17 @@
+package com.greenfoxacademy.springstart.controllers;
+
+public class AtomicLong {
+
+  private long id;
+
+  public AtomicLong() {
+  }
+
+  public AtomicLong(long id) {
+    this.id = id;
+  }
+
+  public long getId() {
+    return id;
+  }
+}
--- week-07/day1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day1/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -2,14 +2,18 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloRESTController {
 
   static Greeting greeting = new Greeting(1, "Hello, ");
+  static AtomicLong atomicLong = new AtomicLong(1);
 
   @RequestMapping(value = "/greeting")
   @ResponseBody
-  public String greeting(@RequestParam("content") String content, @RequestParam("id") long id){
-    return "id: " + id + " content: " + greeting.getContent() + content + "!";
+  public String greeting(@RequestParam(value = "content", defaultValue = "World") String content){
+    return "id: " + atomicLong.incrementAndGet() + " content: " + greeting.getContent() + content + "!";
+
   }
 }
