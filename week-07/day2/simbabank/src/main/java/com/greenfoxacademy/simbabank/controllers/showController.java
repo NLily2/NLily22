b39acbb3048cac6cb79f453458339229ce81d053
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-06-26 15:03:57 +0200

    format balance and submit strings

--- week-07/day2/simbabank/src/main/java/com/greenfoxacademy/simbabank/controllers/showController.java
+++ week-07/day2/simbabank/src/main/java/com/greenfoxacademy/simbabank/controllers/showController.java
@@ -4,6 +4,7 @@ package com.greenfoxacademy.simbabank.controllers;
 import com.greenfoxacademy.simbabank.models.BankAccount;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,14 +12,18 @@ public class showController {
 
   BankAccount bankAccount = new BankAccount("Simba", "2000", "lion");
 
-@RequestMapping("/show")
-  public String show(Model model){
+  @RequestMapping("/show")
+  public String show(Model model) {
     /*model.addAttribute("name", bankAccount.getName());
     model.addAttribute("balance", bankAccount.getBalance());
     model.addAttribute("typeAnimal", bankAccount.getAnimalType()); */
     model.addAttribute("bankaccount", bankAccount);
     return "BankAccountFieldsShowing";
   }
-}
 
-// <!p th:text="'Name: ' + ${name} + ' balance: ' + ${balance} + 'Zebra type of animal: ' + ${typeAnimal}"/>
+  @RequestMapping("/text")
+  public String submitText(Model model) {
+    model.addAttribute("text", "This is an <em>HTML</em> text. <b>Enjoy yourself!</b>");
+    return "submit";
+  }
+}
