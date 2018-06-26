4b4456b31a8c0d7230d5c0a61bc45b1f5bd6eb67
Lilla Nemeth <lily2.freemail.hu@gmail.com>
2018-06-26 12:03:25 +0200

    show one account

---
+++ week-07/day2/simbabank/src/main/java/com/greenfoxacademy/simbabank/controllers/showController.java
@@ -0,0 +1,21 @@
+package com.greenfoxacademy.simbabank.controllers;
+
+
+import com.greenfoxacademy.simbabank.models.BankAccount;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class showController {
+
+  BankAccount bankAccount = new BankAccount("Simba", "2000", "lion");
+
+@RequestMapping("/show")
+  public String show(Model model){
+    model.addAttribute("name", bankAccount.getName());
+    model.addAttribute("balance", bankAccount.getBalance());
+    model.addAttribute("typeAnimal", bankAccount.getAnimalType());
+    return "BankAccountFieldsShowing";
+  }
+}
